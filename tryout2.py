778df709b41b48dd18161a651e7e80ee6b5839ce
poojagupta18 <pujagupta5377@gmail.com>
2018-09-02 22:49:42 +0530

    python learning tryout2 added

---
+++ tryout2.py
@@ -0,0 +1,10 @@
+# This program says high and ask for the age
+
+print ('Hello World!')
+print ('What is your name?') # ask for the name
+myName = input()
+print ('It is good to meet you '+myName)
+print('The length of your name is '+str(len(myName)))
+print('What is your age?') #ask for their age
+myAge = input()
+print ('You will be '+ str(int(myAge) +1 )+' in a year')
