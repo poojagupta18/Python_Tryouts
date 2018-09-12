36641bb61331d6862a9e771e4af8333fd2935c3d
poojagupta18 <pujagupta5377@gmail.com>
2018-09-12 23:00:25 +0530

    User defined function with one argument.

---
+++ tryout6_userDefinedFunction_With_one_argument.py
@@ -0,0 +1,16 @@
+#User defined function have def keyword to define the function.
+#Argument = The value passed in the function call
+#Parameter = The variable inside the function is parameter
+
+def name(name):                  #here name is called parameter
+    print('My name is '+name)
+    print('Hello, '+name)
+
+name('Alice')                   #here Alice is called argument
+name('Bob')
+
+#Output:
+#My name is Alice
+#Hello, Alice
+#My name is Bob
+#Hello, Bob
