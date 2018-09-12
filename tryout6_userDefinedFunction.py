0c2b9321e802d6b82a9595c672b563f0c4e3d11e
poojagupta18 <pujagupta5377@gmail.com>
2018-09-12 22:48:13 +0530

    User defined function with no argument

---
+++ tryout6_userDefinedFunction.py
@@ -0,0 +1,20 @@
+def name():
+    print('Hello world')
+    print('Hey good morning')
+    print('Hey good night')
+
+name()
+name()
+name()
+
+
+#output:
+#Hello world
+#Hey good morning
+#Hey good night
+#Hello world
+#Hey good morning
+#Hey good night
+#Hello world
+#Hey good morning
+#Hey good night
