f275379931d83d8a07eb1a36fde5a58cf084655c
poojagupta18 <pujagupta5377@gmail.com>
2018-09-12 23:09:55 +0530

    Function returning the value using return statement

---
+++ tryout6_functionReturningValue.py
@@ -0,0 +1,10 @@
+#Function returning the value
+
+def onePlus(number):
+    return number + 1        #returning number + 1 value to function
+
+new_number = onePlus(4)      #onePlus(4) will be replaced by 5 and get assigned to new_number
+print(new_number)
+
+#Output:
+#5    
