4e7c38a2cc9d1678b18d0020745aa77bef0fd486
poojagupta18 <pujagupta5377@gmail.com>
2018-09-23 20:26:30 +0530

    type() method

---
+++ tryout_17_type.py
@@ -0,0 +1,19 @@
+#type() method is used to retrieve the datatype with which you are dealing with.
+>>> type(10)
+			
+<class 'int'>
+>>> type('pune')
+			
+<class 'str'>
+>>> type(3.14)
+			
+<class 'float'>
+
+>>> theBoard = {'top-L' : ' ', 'top-M' : ' ', 'top-R' : ' ', 'mid-L' : ' ', 'mid-M' : ' ', 'mid-R' : ' ', 'low-L' : ' ', 'low-M' : ' ', 'low-R' : ' '}			
+>>> type(theBoard)
+
+<class 'dict'>
+>>> type(theBoard['top-L'])
+			
+<class 'str'>
+>>> 
