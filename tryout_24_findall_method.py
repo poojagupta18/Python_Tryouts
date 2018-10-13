611243ca46b21df4f71db887f5440acc8a5dab02
poojagupta18 <pujagupta5377@gmail.com>
2018-10-13 23:28:07 +0530

    About findall()

---
+++ tryout_24_findall_method.py
@@ -0,0 +1,17 @@
+# The regex method findall() is passed a string, and returns all matches in it, not just first match.
+
+# If the regex has no groups(0 or 1), findall() returns a list of string.
+
+# If the regex has 2 or more groups, findall() returns a list of tuples of strings.
+
+# \d is shorthand character class that matches digits.
+
+# \w is shorthand character class that matches characters.
+
+# \s matches whitespaces characters.
+
+# The uppercase shorthand characters \D, \W. \S match characters that are not digits, word characters and spaces.
+
+# You can make your own character classes with [aieuo]
+
+# A ^ caret makes it a negative character class, matching anything not in [^aieou]
